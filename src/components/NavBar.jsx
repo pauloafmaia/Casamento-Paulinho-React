a7fbec17ec4459b49702fde159921d8f8c881ebb
Paulo Maia <pauloafmaia9@gmail.com>
2023-11-12 19:04:53 -0300

    updates

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -56,19 +56,19 @@ function NavBar() {
       </div>
       <div className="w-full h-auto bg-dark-green p-4 hidden lg:block sticky top-0 text-white">
         <ul className="flex flex-row justify-center items-center text-md space-x-6">
-          <NavLink className="font-bold lg:hover:border-b-2" to={"/"}>
+          <NavLink className="font-bold lg:border-b-2 lg:border-b-transparent lg:hover:border-b-white lg:hover:border-b-2" to={"/"}>
             Laura & Paulinho
           </NavLink>
-          <NavLink className="font-bold lg:hover:border-b-2" to={"/local"}>
+          <NavLink className="font-bold lg:border-b-2 lg:border-b-transparent lg:hover:border-b-white lg:hover:border-b-2" to={"/local"}>
             Local e Data
           </NavLink>
-          <NavLink className="font-bold lg:hover:border-b-2" to={"/traje"}>
+          <NavLink className="font-bold lg:border-b-2 lg:border-b-transparent lg:hover:border-b-white lg:hover:border-b-2" to={"/traje"}>
             Traje
           </NavLink>
-          <NavLink className="font-bold lg:hover:border-b-2" to={"/lista"}>
+          <NavLink className="font-bold lg:border-b-2 lg:border-b-transparent lg:hover:border-b-white lg:hover:border-b-2" to={"/lista"}>
             Lista de Presentes
           </NavLink>
-          <NavLink className="font-bold lg:hover:border-b-2" to={"/presenca"}>
+          <NavLink className="font-bold lg:border-b-2 lg:border-b-transparent lg:hover:border-b-white lg:hover:border-b-2" to={"/presenca"}>
             Confirmar Presença
           </NavLink>
         </ul>
